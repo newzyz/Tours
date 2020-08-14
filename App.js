9c01eede3aa9563d94fcb9228f6ef336c817e701
paicrush <dcdc07411@gmail.com>
2020-08-14 23:09:45 +0700

    pai

--- App.js
+++ App.js
@@ -5,13 +5,15 @@
  * @format
  * @flow strict-local
  */
-
-import React from "react";
-import { ImageBackground, StyleSheet, Text, View,Image } from "react-native";
+//hgjhgjl
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View, Image} from 'react-native';
 
 const App = () => (
   <View style={styles.container}>
-    <ImageBackground source={require('./images/image.jpg')} style={styles.image}>
+    <ImageBackground
+      source={require('./images/image.jpg')}
+      style={styles.image}>
       <Text style={styles.text}>Press to Continuous</Text>
     </ImageBackground>
   </View>
@@ -20,19 +22,18 @@ const App = () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column"
+    flexDirection: 'column',
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   text: {
-    color: "grey",
+    color: 'grey',
     fontSize: 30,
-    fontWeight: "bold"
-  }
+    fontWeight: 'bold',
+  },
 });
 
 export default App;
-
